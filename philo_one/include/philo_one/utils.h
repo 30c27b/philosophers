1fd3d9b700dd85beec313092af22056ed062599c
ancoulon <ancoulon@student.s19.be>
2021-06-03 17:17:58 +0200

    working on strtoul implementation + general refactor

--- philo_one/include/philo_one/utils.h
+++ philo_one/include/philo_one/utils.h
@@ -1,4 +1,12 @@
 #ifndef UTILS_H
 # define UTILS_H
 
+# include <stdint.h>
+# include <sys/types.h>
+
+# define STUL_CUTOFF (UINT64_MAX / (uint64_t)10)
+# define STUL_CUTLIM (UINT64_MAX % (uint64_t)10)
+
+uint64_t	utils_strtoul(char *str);
+
 #endif
--- philo_one/source/main/main.c
+++ philo_one/source/main/main.c
@@ -1,5 +1,7 @@
 #include "philo_one.h"
-
+#include <limits.h>
+#include <stdint.h>
+                                                                                                                                                                                                                                                                                
 int	main(void)
 {
 	return (0);
