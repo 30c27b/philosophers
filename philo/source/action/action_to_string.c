567d7e04ba11515b234441b0a3edab3921f5fa19
ancoulon <ancoulon@student.s19.be>
2021-09-09 15:07:26 +0200

    norm

--- philo/source/action/action_to_string.c
+++ philo/source/action/action_to_string.c
@@ -3,7 +3,7 @@
 
 void	action_to_string(t_action action)
 {
-	static char	*strings[5] = {" has taken a fork\n", " is eating\n",
+	static char		*strings[5] = {" has taken a fork\n", " is eating\n",
 		" is sleeping\n", " is thinking \n", " died\n"};
 	static size_t	lens[5] = {18, 11, 13, 14, 6};
 
